113f32759b9c04eee6c228982b6533673b3ddddb
CollinJosh <mubscollin@gmail.com>
2022-04-11 04:07:40 -0700

    task 03

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 /**
- *main - Entry point
+ * main - Entry point
  *
  * Return: Always 0 (Success)
  */
@@ -16,5 +16,5 @@ int main(void);
 
 	putchar('\n');
 	
-	return (0)
+	return (0);
 }
